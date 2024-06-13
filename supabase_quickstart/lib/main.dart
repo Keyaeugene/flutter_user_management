a59c17ee1f0b05659fd01303168e816c28f11e9d
keyaeugene <keyaeugene@gmail.com>
2024-06-13 12:18:23 +0300

    flutter user management

---
+++ supabase_quickstart/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_quickstart/pages/account_page.dart';
+import 'package:supabase_quickstart/pages/login_page.dart';
+import 'package:supabase_quickstart/pages/splash_page.dart';
+
+void main() async {
+  await Supabase.initialize(
+    url: 'https://dtwixoxqodtwseuoldlc.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0d2l4b3hxb2R0d3NldW9sZGxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc3NjAyMjQsImV4cCI6MjAzMzMzNjIyNH0.hN0LNBnUPR-X3nICWBDk_jTXwEtnrENVXAOm1402mIA',
+  );
+  runApp(const MyApp());
+}
+
+final supabase = Supabase.instance.client;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Supabase Flutter',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.green,
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.green,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.green,
+          ),
+        ),
+      ),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (context) => const SplashPage(),
+        '/login': (context) => const LoginPage(),
+        '/account': (context) => const AccountPage(),
+      },
+    );
+  }
+}
